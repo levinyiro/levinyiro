5ff74ccbdc06bcfc7fae17f9e3f5c2468f39863c
leventenyiro <nyiro.levente@gmail.com>
2023-07-08 13:27:32 +0200

    hide paris after pic and modify scroll-smooth

--- portfolio/script/paris.js
+++ portfolio/script/paris.js
@@ -1,11 +1,14 @@
 window.addEventListener('scroll', function () {
     var title = document.getElementById('paris-title');
     var imageContainer = document.querySelector('.image-container');
+    var parisPic = this.document.querySelector('.paris-pictures');
 
     var scrollPosition = window.scrollY;
     var titleOffset = imageContainer.offsetTop - window.innerHeight + 300;
+    var parisPicOffset = parisPic.offsetTop - window.innerHeight + 350;
 
-    if (scrollPosition >= titleOffset) {
+    if (scrollPosition >= titleOffset && scrollPosition < parisPicOffset) {
+        console.log(true);
         var distance = scrollPosition - titleOffset;
         title.style.transform = 'translateY(' + distance + 'px)';
     } else {
--- portfolio/script/scroll-smooth.js
+++ portfolio/script/scroll-smooth.js
@@ -1,12 +1,16 @@
-const lenis = new Lenis()
-
-// lenis.on('scroll', (e) => {
-//     console.log(e)
-// })é
-
-function raf(time) {
-    lenis.raf(time * 0.6)
+const lenis = new Lenis({
+    duration: 1.5, 
+    easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)), // https://easings.net
+      direction: 'vertical', 
+    smooth: true,
+    smoothTouch: false,
+    touchMultiplier: 1.5,
+  })
+  
+  function raf(time) {
+    lenis.raf(time)
     requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
+  }
+  
+  requestAnimationFrame(raf)
+  
