f9c77ba37a0c856961c62d9ca3e5e1b1c0f6b353
leventenyiro <nyiro.levente@gmail.com>
2023-07-12 07:19:14 +0200

    modify scroll-text

--- portfolio/script/scroll-text.js
+++ portfolio/script/scroll-text.js
@@ -16,13 +16,41 @@ window.addEventListener('scroll', function () {
     console.log('Scroll speed:', scrollSpeed);
 });
 
-gsap.to('.milan-title', {
-    x: '-100%',
+// gsap.to('.milan-title', {
+//     x: '-300%',
+//     ease: 'power1.inOut',
+//     duration: 0.000001,
+//     scrollTrigger: {
+//         markers: true,
+//         trigger: '.main',
+//         start: 'top top', // Adjust the start value to 'top center'
+//         end: 'bottom top', // Adjust the end value to 'bottom center'
+//         scrub: true
+//     }
+// });
+
+const titleTimeline = gsap.timeline({ repeat: -1 });
+
+// Add an animation to the timeline
+titleTimeline.to('.milan-title', {
+    x: '-300%',
     ease: 'power1.inOut',
-    scrollTrigger: {
-        trigger: '.container',
-        start: 'top bottom',
-        end: 'bottom top',
-        scrub: true
-    }
+    duration: 10, // Adjust the duration to control the speed of the movement
 });
+
+gsap.to('.milan-title', {
+    x: '-300%',
+    ease: 'power1.inOut',
+    duration: 10, // Adjust the initial duration to control the speed of automatic movement
+    // repeat: -1, // Repeat the animation infinitely
+    // yoyo: true, // Reverse the animation on each repeat
+    scrollTrigger: {
+        markers: true,
+        trigger: '.main',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: true,
+        onEnter: () => gsap.to('.milan-title', { duration: 0.5 }), // Adjust the duration to control the speed when scrolling
+    },
+});
+
