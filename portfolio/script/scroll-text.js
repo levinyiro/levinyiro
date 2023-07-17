00b622361932841f86f35ce622889479666ab59a
leventenyiro <nyiro.levente@gmail.com>
2023-07-17 16:10:55 +0200

    add changes in scroll text

--- portfolio/script/scroll-text.js
+++ portfolio/script/scroll-text.js
@@ -1,12 +1,11 @@
 gsap.to('.malta-title', {
-    x: '-300%',
+    x: '-130%',
     ease: 'power1.inOut',
     duration: 10,
     scrollTrigger: {
-        markers: true,
-        trigger: '.main',
-        start: 'top top',
-        end: 'bottom top',
+        trigger: '.malta-title',
+        start: 'center center',
+        end: 'bottom center',
         scrub: true,
         onEnter: () => gsap.to('.malta-title', { duration: 0.5 }),
     },
