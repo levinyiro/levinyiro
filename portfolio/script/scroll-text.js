08cfa4ed2583b5d5a155fa8ed563e8c9b80263c9
leventenyiro <nyiro.levente@gmail.com>
2023-07-30 15:08:10 +0200

    add malta video

--- portfolio/script/scroll-text.js
+++ portfolio/script/scroll-text.js
@@ -10,3 +10,17 @@ gsap.to('.malta-title', {
         onEnter: () => gsap.to('.malta-title', { duration: 0.5 }),
     },
 });
+
+gsap.to('.explore-title', {
+    y: '-250%',
+    ease: 'power1.inOut',
+    duration: 10,
+    scrollTrigger: {
+        markers: true,
+        trigger: '.malta-title',
+        start: 'center top',
+        end: 'bottom top',
+        scrub: true,
+        onEnter: () => gsap.to('.explore-title', { duration: 0.5 }),
+    },
+})
